24bba1e76fe1bc3fbddbc632331567dbd8c004d5
fxbite <letruongthinh1703@gmail.com>
2022-02-15 22:17:10 +0700

    Change style route of idea

--- src/routes/idea.js
+++ src/routes/idea.js
@@ -10,10 +10,11 @@ router.put('/update/:id', ideaController.updateIdea)
 // delete a idea
 router.delete('/delete/:id', ideaController.deleteIdea)
 
-// get all idea
-router.get('/all', ideaController.getAllIdea)
-
 // get a idea
 router.get('/one/:id', ideaController.getAIdea)
+
+// get all idea
+router.get('/', ideaController.getAllIdea)
+
 
 module.exports = router
