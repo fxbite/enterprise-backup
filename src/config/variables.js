8b33a339a46fd604e6c03d4ca994244d3248da52
fxbite <letruongthinh1703@gmail.com>
2022-02-19 22:11:39 +0700

    Add refresh token drive

--- src/config/variables.js
+++ src/config/variables.js
@@ -4,10 +4,13 @@ module.exports = {
    MONGO_URI: 'mongodb+srv://fxbite:Rq6V3hdiAYpqWhOx@cluster0.xc5u8.mongodb.net/cms?retryWrites=true&w=majority', 
 
 
-   // GOOGLE API
+   // GOOGLE GMAIL API
    CLIENT_ID: '804342104664-epflhhfjvaauj6clads4aq4djb697520.apps.googleusercontent.com',
    CLIENT_SECRET: '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',
    REDIRECT_URI: 'https://developers.google.com/oauthplayground',
-   REFRESH_TOKEN: '1//04PsB2rA5u8NrCgYIARAAGAQSNwF-L9Irv_govqHQMmrn5zxuQJkowguTl_iIpRkSED7aIwC1f6rFvCKnvH1ZaYrD4pM6mAdBIV0'
+   REFRESH_TOKEN: '1//04PsB2rA5u8NrCgYIARAAGAQSNwF-L9Irv_govqHQMmrn5zxuQJkowguTl_iIpRkSED7aIwC1f6rFvCKnvH1ZaYrD4pM6mAdBIV0',
 
+   // GOOGLE DRIVE API
+   REFRESH_TOKEN_DRIVE: '1//04dj5loXpzu6TCgYIARAAGAQSNwF-L9IrFvWUBY0KQ9LB8W5jNP3gsKFBYhCIAQMC_If-vui6jRp_j1eirXJZfwbwAsPFDL8T-vI'
+  
 }
